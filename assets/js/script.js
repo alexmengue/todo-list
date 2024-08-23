85d0a9493d2693b96d94479c27186adcf1df0d3d
Alex <alexmengue1990@hotmail.com>
2024-08-23 19:58:13 -0300

    getting input value

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
 const button = document.querySelector('.button-add-task');
 const input = document.querySelector('.input-task');
 
+button.addEventListener('click', () => {
+  console.log(input.value);
+});
