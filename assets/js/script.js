bfe29631a8bbf2732c6b6b92146be5638230e475
Alex <alexmengue1990@hotmail.com>
2024-09-09 00:57:51 -0300

    fixed some mistake

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,8 +18,8 @@ function showTasks() {
 
   tasksList.forEach((item, index) => {
     newLi = newLi + `
-      <li class="task">
-        <img class="${item.concluded && 'done'}" src="assets/img/checked.png" alt="tarefa marcada" onclick="concludeTask(${index})" />
+      <li class="task ${item.concluded && "done"}">
+        <img src="assets/img/checked.png" alt="tarefa marcada" onclick="concludeTask(${index})" />
         <p>${item.task}</p>
         <img src="assets/img/trash.png" alt="tarefa excluída" onclick="deleteItem(${index})" />
       </li>
